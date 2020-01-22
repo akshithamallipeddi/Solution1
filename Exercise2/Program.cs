f1107d419a11b871cd1742402e0f80167212505c
amallipeddi <akshitha>
2020-01-21 21:01:42 -0500

    exercise2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int age, height;
+			//Max heart rate is equal 220-age
+
+			age = 46;
+			height = 60;
+
+			//Console should write: I am ### years old, my max heart rate is ## and I an ## inches tall.
+			Console.Write("I am " + age + " years old, my max heart rate is " + (220 - age) + " and I am " + height + " inches tall.");
+		}
+	}
+}
